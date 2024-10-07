46bb35beaa11c5a47970694d496fd0ebd7f13ce7
Kasten33 <kadenhurley5@gmail.com>
2024-10-06 21:12:25 -0600

    page for the character sheet it working

--- components/CharacterSheet/CharaSheet.jsx
+++ components/CharacterSheet/CharaSheet.jsx
@@ -1,4 +1,5 @@
-import styles from "./CS.module.scss";
+import React from 'react';
+import styles from './CS.module.scss';
 
 const stats = [
   "Strength",
@@ -9,6 +10,7 @@ const stats = [
   "Charisma",
 ];
 const chara = ["class", "subclass", "race", "subrace", "background"];
+
 const CharaSheet = () => {
   // Function to generate table rows dynamically
   const generateAttributes = () => {
@@ -16,7 +18,7 @@ const CharaSheet = () => {
       <tr key={stat}>
         <td className={styles.td}>{stat}</td>
         <td className={styles.td}>
-          <input type="number" className={styles.statInput} /> 
+          <input type="number" className={styles.statInput} />
         </td>
         <td className={styles.td}>0</td>
       </tr>
@@ -40,39 +42,39 @@ const CharaSheet = () => {
 
   return (
     <div>
-      <p className={styles.header}>Character Sheet</p>
+      <p className={styles.header}>Header</p>
       <div className={styles.container}>
         <div className={styles.box}>
-          <table className={styles.table}>
+          <table>
             <thead>
               <tr>
-                <th colSpan="6" className={styles.th}>Character Info</th>
+                <th colSpan="6">Character Info</th>
               </tr>
               <tr>
-                <th className={styles.th}>Level</th>
-                <th className={styles.th}>
+                <th>Level</th>
+                <th>
                   <input type="number" className={styles.statInput} />
                 </th>
-                <th className={styles.th}>PB: num</th>
-                <th className={styles.th}>INSP: YES</th>
+                <th>PB: num</th>
+                <th>INSP: YES</th>
               </tr>
             </thead>
             <tbody id="chara-info">{generateCharaInfo()}</tbody>
           </table>
         </div>
         <div className={styles.box}>
-          <table className={styles.table}>
+          <table>
             <thead>
-            <tr>
-              <th className={styles.th}>Stat</th>
-              <th className={styles.th}>Value</th>
-              <th className={styles.th}>Modifier</th>
-            </tr>
+              <tr>
+                <th>Stat</th>
+                <th>Value</th>
+                <th>Modifier</th>
+              </tr>
             </thead>
             <tbody id="stats-table">{generateAttributes()}</tbody>
           </table>
         </div>
-        <div className={styles.box}></div>
+        <div className={styles.box}>Two</div>
         <div className={styles.box}>Three</div>
         <div className={styles.box}>Four</div>
       </div>
